fe4e0f76253bd52aa5525ec76dd99f7db7281f47
jakepatolilic <jakepatolilic@gmail.com>
2024-05-19 14:12:12 +0800

    pdf conversion feature improved, and historyDetailPage improved

--- public/historyDetails.js
+++ public/historyDetails.js
@@ -21,41 +21,37 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch(`/history/${historyId}`)
             .then(response => response.json())
             .then(data => {
-                const { historyDetails, equipmentDetails } = data;
+                const { historyDetails, equipmentDetails, areaDetails } = data;
 
                 const actualDate = new Date(historyDetails.actualDate).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-            
+                const proposedDate = new Date(historyDetails.proposedDate).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
                 document.getElementById('equip-img').innerHTML = `
-                    <img  src="${equipmentDetails.equip_pic}" alt="equipment-picure">
+                    <img src="${equipmentDetails.equip_pic}" alt="equipment-picture">
                 `;
 
                 document.getElementById('planned-date').innerHTML = `
-                    <p style="color: red;"><strong>Date Planned: ${actualDate}</strong></p>
-                    <p><strong>Activity:</strong> xd</p>
-                    <p><strong>Findings:</strong> xd</p>
-                    <p><strong>Identified Problem:</strong> xd</p>
-                    <p><strong>Replacement Parts:</strong> xd</p>
+                    <p style="color: red;"><strong>Date Planned: ${proposedDate}</strong></p>
+                    <p><strong>Remarks:</strong>${historyDetails.remarks1}</p>
                 `;
 
                 document.getElementById('actual-date').innerHTML = `
                     <p style="color: green;"><strong>Maintenance Date: ${actualDate}</strong></p>
-                    <p><strong>Activity:</strong> PC Monitor Replacement</p>
-                    <p><strong>Problem Fixed:</strong> xd</p>
-                    <p><strong>Performance:</strong> good</p>
+                    <p><strong>Result:</strong> ${historyDetails.remarks2}</p>
                 `;
-        
+
                 document.getElementById('equip-details').innerHTML = `
-                    <p><strong>Equipment Name:</strong></p>
-                    <p><strong>Asset ID:</strong> Ceiling fan</p>
-                    <p><strong>Model No.:</strong> Ceiling fan</p>
-                    <p><strong>Serial Number:</strong> Ceiling fan</p>
-                    <p><strong>Floor:</strong> Ceiling fan</p>
-                    <p><strong>Area:</strong> Ceiling fan</p
+                    <p><strong>Equipment Name:</strong> ${historyDetails.equip_name}</p>
+                    <p><strong>Equipment Status:</strong> ${historyDetails.status}</p>
+                    <p><strong>Serial No:</strong> ${historyDetails.equip_no}</p>
+                    <p><strong>Area:</strong> ${areaDetails.area_name}</p>
+                    <p><strong>Floor:</strong> ${areaDetails.floor_name}</p>
                 `;
             })
             .catch(error => console.error('Error fetching data:', error));
     }
 });
+
 
 function checkUserLoggedIn() {
     const cookies = document.cookie.split('; ');
@@ -96,6 +92,7 @@ document.getElementById('generate-pdf-btn').addEventListener('click', async () =
         console.error('ID not found in the URL');
         return;
     }
+
     const pdfUrl = `/pdf/${historyId}`;
 
     try {
@@ -109,7 +106,7 @@ document.getElementById('generate-pdf-btn').addEventListener('click', async () =
             link.setAttribute('download', 'history-details.pdf');
             document.body.appendChild(link);
             link.click();
-            link.parentNode.removeChild(link);
+            document.body.removeChild(link);
         } else {
             console.error('Failed to generate PDF.');
         }
@@ -117,6 +114,7 @@ document.getElementById('generate-pdf-btn').addEventListener('click', async () =
         console.error('Error generating PDF:', error);
     }
 });
+
 
 function logout() {
     const cookiePaths = [
